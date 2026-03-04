9890ca581a5472bf3f963877c1e191a3d73776c9
William-Dionne-B <2349786@cegeplimoilou.ca>
2026-03-04 14:08:16 -0500

    Menu Fonctionnel

--- Assets/Scripts/MainMenuManager.cs
+++ Assets/Scripts/MainMenuManager.cs
@@ -1,16 +1,25 @@
+using System.Security.Cryptography;
 using UnityEngine;
 
 public class MainMenuManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public void OnStartButtonPressed()
     {
-        
+        loadScene("SystemeSolaire");
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnQuitButtonPressed()
     {
-        
+        #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+        #else
+            Application.Quit();
+        #endif
     }
+
+    public void loadScene(string sceneName)
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(sceneName);
+    }
+
 }
