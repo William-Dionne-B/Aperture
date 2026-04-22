594d947d7fd3fda92e1dbe2d17400d3c4edee6e2
SamuelGauthier0 <Samuel.Gauthier@etudiant.cegeplimoilou.ca>
2026-04-22 17:20:24 -0400

    correction du soleil qui reviens du gym

--- Assets/Scripts/Gravity/GravityBody.cs
+++ Assets/Scripts/Gravity/GravityBody.cs
@@ -6,8 +6,8 @@ public class GravityBody : MonoBehaviour
     [Header("Initial Motion")]
     public Vector3 initialVelocity;
     public bool applyInitialVelocity = true;
-    
-    [HideInInspector]public float Mass = 1f;
+
+    [HideInInspector] public float Mass = 1f;
     [HideInInspector] public Rigidbody rb;
     [HideInInspector] public LineRenderer line;
 
